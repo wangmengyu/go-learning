be2d1f8ab1b4ceb0d6bd7aac54cce236c5d78a7a
mengyuwang <mengyu.wang.0124@gmail.com>
2019-11-20 10:56:51 +0800

    channel goroutine done

---
+++ channel/done/done.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/**
+worker 结构，
+包含两个参数,
+一个用于接收数据的channel，
+一个用于完成done通知动作的function
+
+
+*/
+type worker struct {
+	in   chan int
+	done func()
+}
+
+/**
+创建work
+*/
+func createWork(i int, wg *sync.WaitGroup) worker {
+	//定义一个channel, 和go func 不断读取channel中的数据
+	w := worker{
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
+	}
+	go doWork(i, w)
+	return w
+}
+
+func chanDemo() {
+
+	//定义10个元素的协程数组
+	var workers [10]worker
+
+	var wg sync.WaitGroup
+	wg.Add(20) // 等待20个任务被完成
+
+	for i := 0; i < 10; i++ {
+		workers[i] = createWork(i, &wg)
+	}
+
+	//内置等待工具
+	for i, work := range workers {
+		work.in <- 'a' + i
+	}
+
+	for i, work := range workers {
+		work.in <- 'A' + i
+	}
+
+	//执行等待
+	wg.Wait()
+
+}
+
+/**
+创建work , 接收来自缓冲类channel的数据
+done 参数， 当执行完成后，使用channel通知完成
+*/
+func doWork(id int, w worker) {
+	for n := range w.in {
+		fmt.Printf("work %d, received %c\n", id, n)
+		//需要在开个goroutine， 不会卡住
+		w.done()
+
+	}
+	fmt.Println("no data")
+}
+
+func main() {
+
+	chanDemo()
+
+}
