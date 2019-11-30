182a4491cb26fe39a33cd93eea89d5caf6584579
wangmengyu <mengyu.wang.0124@gmail.com>
2019-11-30 16:55:33 +0800

    选择排序

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -13,38 +13,19 @@ func main() {
   冒泡排序
 */
 func bubbleSort(queue []int) []int {
-	p1 := len(queue) - 1
-	p2 := len(queue) - 2
-	res := make([]int, 0) //存储结果切片
-	fmt.Println("p1,p2 = ", p1, p2)
-	for len(queue) > 0 { //检查是否要处理的数据还有
-		//定义最右侧的两个指针，分别指向最后两个元素
-		p1 := len(queue) - 1
-		p2 := len(queue) - 2
-		for p1 >= 1 {
-			//每次检查连个指针指向的数字
-			if queue[p2] > queue[p1] {
-				//如果左指针指向的数大于右边的，就交换
-				fmt.Println(queue[p1], queue[p2])
-				queue[p1], queue[p2] = change(queue[p1], queue[p2])
-				fmt.Println(queue[p1], queue[p2])
-			} else {
-				//否则不做任何操作
-				fmt.Println("ok sort:", queue[p1], queue[p2])
+	totalLen := len(queue)
+	for i := 0; i < totalLen-1; i++ {
+		//从后向前依次访问相邻节点，如果左边大于右边，则进行交换
+		p1 := totalLen - 2
+		p2 := totalLen - 1
+		for p1 >= i {
+			if queue[p1] > queue[p2] {
+				queue[p1], queue[p2] = queue[p2], queue[p1]
 			}
-			//两个指针继续向左移动一格
 			p1 = p1 - 1
 			p2 = p2 - 1
-			fmt.Println("p1,p2 = ", p1, p2)
 		}
-		//将第一个元素是当前序列最小的数字，追加到返回序列。
-		res = append(res, queue[0])
-		queue = queue[1:]
+		//每次循环处理后会将最小节点换到当前比较序列最左边，固定下来，位置=i，再用同样方式排序好后续的节点
 	}
-	return res
-}
-
-func change(i int, i2 int) (int, int) {
-	return i2, i
-
+	return queue
 }
--- selectsorting/selectsorting.go
+++ selectsorting/selectsorting.go
@@ -3,46 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	myList := []int{4, 3, 8, 6}
+	myList := []int{6, 2, 1, 7, 9, 3, 4, 8, 5}
 	myList = selectSort(myList)
 	fmt.Println(myList)
 
 }
 
 /**
-选择排序。
-从序列中，寻找最小的元素，挑出来，放入新的序列。
-直到全部挑完
+选择排序
 */
 func selectSort(myList []int) []int {
-	var res []int
-	for len(myList) > 0 {
-		//寻找最小的元素 和他 的位置
-		minVal, pos := findMin(myList)
-		//前半部分和后半部分序列拼接起来 ， 除了找到的最小元素
-		endList := myList[pos+1:]
-		myList = myList[:pos]
-		for _, v := range endList {
-			myList = append(myList, v)
+	//每次遍历要处理的序列
+	for i := 0; i < len(myList); i++ {
+		//从当前未处理队列中找出值最小的，与i位置的值进行交换
+		for j := i + 1; j < len(myList); j++ {
+			if myList[j] < myList[i] {
+				myList[j], myList[i] = myList[i], myList[j]
+			}
 		}
-		res = append(res, minVal)
+		//一次循环完毕后，未处理序列最小的值会出现再i位置。
 	}
 
-	return res
-
-}
-
-/**
-查找序列中最小的元素值和他的位置
-*/
-func findMin(myList []int) (int, int) {
-	minVal := myList[0]
-	pos := 0
-	for i, v := range myList {
-		if v < minVal {
-			minVal = v
-			pos = i
-		}
-	}
-	return minVal, pos
+	return myList
 }
